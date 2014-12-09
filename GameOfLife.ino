7de44d60468eafb8d9aab7823aa32767644ad0e7
Art Dahm <art@dragongears.com>
2014-12-08 20:53:59 -0800

    Comments

--- GameOfLife.ino
+++ GameOfLife.ino
@@ -1,7 +1,31 @@
 /*
-	SmartMatrix Game of Life
-*/
+ * SmartMatrix Game of Life - Conway's Game of Life for the Teensy 3.1 and SmartMatrix Shield.
+ *
+ * Copyright (c) 2014 Art Dahm (art@dahm.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
+// The array containing the cells expands beyond the display by one cell in each direction.
+// The cells on the edge of the display are copied to the opposite side before the next
+// generation is calculated to allow the field to wrap (Instead of doing a test for each
+// cell's calculation).
+//
 // 9|789|7
 // -+---+-
 // 3|123|1
